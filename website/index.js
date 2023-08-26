c0aa25e37870f416b2b645f7be5979488f2eb0a9
ellencrombie123 <ellencrombie123>
2023-08-26 15:41:45 +0000

    partial ci cd

--- website/index.js
+++ website/index.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".count-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://fmwc65m4bn7wczvxysuznk6vqy0ztczn.lambda-url.us-east-1.on.aws/"
+        "https://dplnfxjcnp7juyn5lhs55qfnse0aysks.lambda-url.us-east-1.on.aws/"
         );
     let data = await response.json();
     counter.innerHTML = ` Page view count: ${data}`;
